3441efb1565931ad7f06e9d102ce30320ca1f3c4
Yichun Sun <sunyichun624@gmail.com>
2021-05-10 21:41:50 +0000

    challenge2

---
+++ challenge1/challenge02.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+icecream =["flavors","salty"]
+
+icecream.append(99)
+
+name = input("Name please?\n>")
+
+print(f"<{icecream[-1]}> <{icecream[0]}>, and {name} choose to be <{icecream[1]}>")
