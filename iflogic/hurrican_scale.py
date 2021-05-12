1bddeef538313923252ef7f0f60e793f1938ba8d
Yichun Sun <sunyichun624@gmail.com>
2021-05-12 02:46:20 +0000

    add damage data

--- iflogic/hurrican_scale.py
+++ iflogic/hurrican_scale.py
@@ -1,19 +1,31 @@
 #! usr/bin/env python3
+import hurrican_data as data
 
 def hurrican_scale(speed):
     prefix = "\nThe category of the hurrican is "
+    
     if speed<74:
         print("\nIt is not a hurrican.")
+    
     elif 74<speed<95:
         print(prefix+"ONE.")
+        data.print_damage("ONE")
+    
     elif 96<speed<110:
         print(prefix+"TWO.")
+        data.print_damage("TWO")
+
     elif 111<speed<129:
         print(prefix+"THREE.")
+        data.print_damage("THREE")
+
     elif 130<speed<156:
         print(prefix+"FOUR.")
+        data.print_damage("FOUR")
+
     else:
         print(prefix+"FIVE.")
+        data.print_damage("FIVE")
 
 while True:
     try:
